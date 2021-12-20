7a38cd5b512d3694b002590d49b2acb6af62497e
jcarnec <carnecj@tcd.ie>
2021-12-20 18:31:31 +0000

    m

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ const languageNames = new Intl.DisplayNames(["en"], {
   type: "language",
 });
 const API_KEY = "e316601ca43413563469752bc6096a5b";
-const TABLE_PATH = "/data/devMovies.csv";
-// const TABLE_PATH = "/data/movie_results.csv";
+// const TABLE_PATH = "/data/devMovies.csv";
+const TABLE_PATH = "/data/movie_results.csv";
 const MAX_POPULARITY = 150;
 let barInfoBeingDisplayed = null;
 let twcb;
